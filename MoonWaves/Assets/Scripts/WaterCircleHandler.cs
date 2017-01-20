81b35556571b33e35200aea9fe73901a5bf4e3e3
Ensutee <magnus_s_n@hotmail.com>
2017-01-20 22:05:46 +0100

    Messed with the forces to avoid orbiting water

--- MoonWaves/Assets/Scripts/WaterCircleHandler.cs
+++ MoonWaves/Assets/Scripts/WaterCircleHandler.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterCircleHandler : MonoBehaviour {
+
+    [Header("Settings")]
     public int CirclesToSpawn;
-    public GameObject CirclePrefab;
-
-    public GameObject ForceTargetPlanet;
-    public GameObject ForceTargetMoon;
-
     public float PullForcePlanet;
     public float PullForceMoon;
+    public float maxDistFromPlanet;
+
+    [Header("References")]
+    public GameObject CirclePrefab;
+    public GameObject ForceTargetPlanet;
+    public GameObject ForceTargetMoon;
 
     private Rigidbody2D[] Circles;
 
@@ -20,9 +23,20 @@ public class WaterCircleHandler : MonoBehaviour {
 	void Start () {
         SpawnCircles(CirclesToSpawn);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void SpawnCircles(int count)
+    {
+        Circles = new Rigidbody2D[CirclesToSpawn];
+
+        for (int i = 0; i < count; i++)
+        {
+            Rigidbody2D g = Instantiate(CirclePrefab, new Vector2(Random.Range(-10f, 10f), Random.Range(-5f, 5f)), Quaternion.identity, transform).GetComponent<Rigidbody2D>();
+            Circles[i] = g;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 	    count++;
 	    if (count%1 == 0) {
 	        for (int i = 0; i < CirclesToSpawn; i++) {
@@ -32,17 +46,18 @@ public class WaterCircleHandler : MonoBehaviour {
 	}
 
     private void ApplyForces(Rigidbody2D target) {
-        Vector2 dirPlanet = (ForceTargetPlanet.transform.position - target.transform.position).normalized;
-        Vector2 dirMoon = (ForceTargetMoon.transform.position - target.transform.position).normalized;
-        target.AddForce(dirPlanet * PullForcePlanet + dirMoon * PullForceMoon);
+        Vector2 dirPlanet = ForceTargetPlanet.transform.position - target.transform.position;
+        bool getPulled = dirPlanet.magnitude < maxDistFromPlanet;
+        target.drag = getPulled ? .25f : 0;
+
+        Vector2 dirMoon = ForceTargetMoon.transform.position - target.transform.position;
+
+        target.AddForce(dirPlanet.normalized * PullForcePlanet + dirMoon.normalized * (getPulled ? PullForceMoon : 0));
     }
 
-    private void SpawnCircles(int count) {
-        Circles = new Rigidbody2D[CirclesToSpawn];
-
-        for (int i = 0; i < count; i++) {
-            Rigidbody2D g = Instantiate(CirclePrefab, new Vector2(Random.Range(-10f, 10f), Random.Range(-5f, 5f)), Quaternion.identity, transform).GetComponent<Rigidbody2D>();
-            Circles[i] = g;
-        }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 1, 0, .25f);
+        Gizmos.DrawSphere(ForceTargetPlanet.transform.position, maxDistFromPlanet);
     }
 }
--- MoonWaves/Assets/Shader/FluidShader.shader
+++ MoonWaves/Assets/Shader/FluidShader.shader
@@ -48,6 +48,12 @@
 			fixed4 frag (v2f i) : SV_Target
 			{
 				fixed4 col = tex2D(_MainTex, i.uv);
+
+				//potentially flip uv.y
+				#if UNITY_UV_STARTS_AT_TOP
+				i.uv.y = 1 - i.uv.y;
+				#endif
+				
 				fixed4 fluid = tex2D(_FluidTex, i.uv);
 				
 				if (fluid.r > _Cutoff || fluid.g > _Cutoff || fluid.b > _Cutoff) {
