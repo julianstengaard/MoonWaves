293c797243c700513f1b93fd5bcd06835e232528
Julian Stengaard <julianstengaard@gmail.com>
2017-01-20 22:09:17 +0100

    mere

--- MoonWaves/Assets/Scripts/MoonMover.cs
+++ MoonWaves/Assets/Scripts/MoonMover.cs
@@ -1,33 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoonMover : MonoBehaviour {
+    public string InputAxisName;
+    public float InitialPIAngle;
+
     public GameObject Anchor;
     public float Distance;
 
     public AnimationCurve SpeedCurve;
     public float BrakeFactor;
+    public AnimationCurve BounceCurve;
 
     private float _curveDuration;
 
     private float _currentInertia;
     private float _currentAngle;
 
+    private float _bouncing;
+    private float _bounceTimer;
+
 	// Use this for initialization
 	void Start () {
 	    _curveDuration = SpeedCurve[SpeedCurve.length - 1].time;
+	    _currentAngle = InitialPIAngle * Mathf.PI;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    float horizontal = Input.GetAxis("Horizontal");
+	    float horizontal = Input.GetAxis(InputAxisName);
 	    if (Mathf.Abs(horizontal) > 0.1f) {
             UpdateInertia(horizontal, true);
         } else {
             UpdateInertia(horizontal, false);
         }
-        UpdateMovement();
+        UpdateAngle();
         SetPosition(_currentAngle);
     }
 
@@ -44,13 +53,25 @@ public class MoonMover : MonoBehaviour {
         }
     }
 
-    private void UpdateMovement() {
+    private void UpdateAngle() {
         _currentAngle += Mathf.Sign(_currentInertia) * SpeedCurve.Evaluate(_currentInertia) * Time.deltaTime;
+        if (_currentAngle >= Mathf.PI * 2f) {
+            _currentAngle -= Mathf.PI * 2f;
+        } else if (_currentAngle <= -Mathf.PI * 2f) {
+            _currentAngle += Mathf.PI * 2f;
+        }
     }
 
     private void SetPosition(float angle) {
         float xPos = Mathf.Cos(angle);
         float yPos = Mathf.Sin(angle);
         transform.position = new Vector3(xPos, yPos) * Distance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (!collision.isTrigger) {
+            return;
+        }
+        print("lol");
     }
 }
