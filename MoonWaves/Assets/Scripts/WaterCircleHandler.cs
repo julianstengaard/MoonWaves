97e6dad22205af49cc2f60ca74a8fd7c72425364
Julian Stengaard <julianstengaard@gmail.com>
2017-01-20 23:08:26 +0100

    d

--- MoonWaves/Assets/Scripts/WaterCircleHandler.cs
+++ MoonWaves/Assets/Scripts/WaterCircleHandler.cs
@@ -10,6 +10,9 @@ public class WaterCircleHandler : MonoBehaviour {
     public float PullForceMoon;
     public float outOfBoundsDist;
 
+    public float MoonForceRange;
+    public AnimationCurve MoonForceFalloff; 
+
     [Header("OutOfBounds")]
     public float oobPlanetForceMod;
     public float oobParticleDrag;
@@ -17,7 +20,8 @@ public class WaterCircleHandler : MonoBehaviour {
     [Header("References")]
     public GameObject CirclePrefab;
     public GameObject ForceTargetPlanet;
-    public GameObject ForceTargetMoon;
+    public GameObject ForceTargetMoon1;
+    public GameObject ForceTargetMoon2;
 
     private Rigidbody2D[] Circles;
 
@@ -28,12 +32,10 @@ public class WaterCircleHandler : MonoBehaviour {
         SpawnCircles(CirclesToSpawn);
 	}
 
-    private void SpawnCircles(int count)
-    {
+    private void SpawnCircles(int count) {
         Circles = new Rigidbody2D[CirclesToSpawn];
 
-        for (int i = 0; i < count; i++)
-        {
+        for (int i = 0; i < count; i++) {
             Rigidbody2D g = Instantiate(CirclePrefab, new Vector2(Random.Range(-10f, 10f), Random.Range(-5f, 5f)), Quaternion.identity, transform).GetComponent<Rigidbody2D>();
             Circles[i] = g;
         }
@@ -51,20 +53,25 @@ public class WaterCircleHandler : MonoBehaviour {
 
     private void ApplyForces(Rigidbody2D target) {
         Vector2 dirPlanet = ForceTargetPlanet.transform.position - target.transform.position;
+        Vector2 dirMoon1 = ForceTargetMoon1.transform.position - target.transform.position;
+        Vector2 dirMoon2 = ForceTargetMoon2.transform.position - target.transform.position;
+
+        Vector2 dirMoon1Scaled = dirMoon1.normalized * MoonForceFalloff.Evaluate(Mathf.InverseLerp(0f, MoonForceRange, dirMoon1.magnitude)) * PullForceMoon;
+        Vector2 dirMoon2Scaled = dirMoon2.normalized * MoonForceFalloff.Evaluate(Mathf.InverseLerp(0f, MoonForceRange, dirMoon2.magnitude)) * PullForceMoon;
+
         bool outOfBounds = dirPlanet.magnitude > outOfBoundsDist;
         target.drag = outOfBounds ? 0 : oobParticleDrag;
-
-        Vector2 dirMoon = ForceTargetMoon.transform.position - target.transform.position;
-
+        
         target.AddForce(
             dirPlanet.normalized * PullForcePlanet * (outOfBounds ? oobPlanetForceMod : 1) +        //Planet force
-            dirMoon.normalized * (outOfBounds ? 0 : PullForceMoon)                  //Moon force
+            dirMoon1Scaled + dirMoon2Scaled                  //Moon force
             );
     }
 
-    void OnDrawGizmos()
-    {
+    void OnDrawGizmos() {
         Gizmos.color = new Color(0, 1, 0, .25f);
         Gizmos.DrawSphere(ForceTargetPlanet.transform.position, outOfBoundsDist);
+        Gizmos.DrawSphere(ForceTargetMoon1.transform.position, MoonForceRange);
+        Gizmos.DrawSphere(ForceTargetMoon2.transform.position, MoonForceRange);
     }
 }
