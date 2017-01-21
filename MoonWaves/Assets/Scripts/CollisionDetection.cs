87fce2849fe31be8c1d2c0f8a8e5926bd68f324c
Julian Stengaard <julianstengaard@gmail.com>
2017-01-21 13:21:45 +0100

    collision counter

---
+++ MoonWaves/Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour {
+    private void OnCollisionStay2D(Collision2D collision) {
+        CollisionCounter.Singleton.AddCollision(transform.position.sqrMagnitude);
+    }
+}
