ddae22f593339072db2c4e70e63038d9b6f493c4
Julian Stengaard <julianstengaard@gmail.com>
2017-01-22 00:14:10 +0100

    IMPLEMENT SUCKING

--- MoonWaves/Assets/Scripts/StuckBallMover.cs
+++ MoonWaves/Assets/Scripts/StuckBallMover.cs
@@ -16,27 +16,27 @@ public class StuckBallMover : MonoBehaviour {
 		
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if (!_ballsFadeTime.ContainsKey(collision.gameObject)) {
-            _ballsFadeTime.Add(collision.gameObject, Time.realtimeSinceStartup + fizzleTime);
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision) {
-        if (!_ballsFadeTime.ContainsKey(collision.gameObject)) {
-            return;
-        }
-        if (Time.realtimeSinceStartup > _ballsFadeTime[collision.gameObject]) {
-            StartCoroutine(BallFader(collision.gameObject));
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision) {
-        if (!_ballsFadeTime.ContainsKey(collision.gameObject)) {
-            return;
-        }
-        _ballsFadeTime.Remove(collision.gameObject);
-    }
+//private void OnTriggerEnter2D(Collider2D collision) {
+//    if (!_ballsFadeTime.ContainsKey(collision.gameObject)) {
+//        _ballsFadeTime.Add(collision.gameObject, Time.realtimeSinceStartup + fizzleTime);
+//    }
+//}
+//
+//private void OnTriggerStay2D(Collider2D collision) {
+//    if (!_ballsFadeTime.ContainsKey(collision.gameObject)) {
+//        return;
+//    }
+//    if (Time.realtimeSinceStartup > _ballsFadeTime[collision.gameObject]) {
+//        StartCoroutine(BallFader(collision.gameObject));
+//    }
+//}
+//
+//private void OnTriggerExit2D(Collider2D collision) {
+//    if (!_ballsFadeTime.ContainsKey(collision.gameObject)) {
+//        return;
+//    }
+//    _ballsFadeTime.Remove(collision.gameObject);
+//}
 
 	public void RemoveBall(GameObject ball)
 	{
@@ -45,8 +45,7 @@ public class StuckBallMover : MonoBehaviour {
 		StartCoroutine(BallFader(ball));
 	}
     private IEnumerator BallFader(GameObject ball) {
-        Vector3 baseScale = ball.transform.localScale;
-        _ballsFadeTime.Remove(ball);
+        Vector3 baseScale = new Vector3(0.03f, 0.03f, 0.03f);
         float endTime = Time.realtimeSinceStartup + 1f;
         while (Time.realtimeSinceStartup < endTime) {
             ball.transform.localScale *= 0.9f;
@@ -56,6 +55,7 @@ public class StuckBallMover : MonoBehaviour {
         float randomAngle = Random.value > 0.5f ? Random.Range(1f, Mathf.PI - 1f) : Random.Range(Mathf.PI + 1f, Mathf.PI * 2f - 1f);
         ball.gameObject.transform.position = new Vector2(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle)) * 2.6f;
 
+        _ballsFadeTime.Remove(ball);
 
         ball.transform.localScale = baseScale;
     }
