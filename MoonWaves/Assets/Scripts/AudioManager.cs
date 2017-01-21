d59944da56e4db307978554836d4ded9422770dc
Ensutee <magnus_s_n@hotmail.com>
2017-01-21 16:00:06 +0100

    Waves ambience audio

---
+++ MoonWaves/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	[Header("Ambience Settings")]
+	public float fadeTime;
+	public float wavesTwoSwitchCount;
+	public float wavesThreeSwitchCount;
+
+	[Header("Ambience")]
+	public AudioSource wavesOne;
+	public AudioSource wavesTwo;
+	public AudioSource wavesThree;
+
+	void Start()
+	{
+		wavesOne.volume = 0;
+		wavesOne.Play();
+		wavesTwo.volume = 0;
+		wavesTwo.Play();
+		wavesThree.volume = 0;
+		wavesThree.Play();
+
+	}
+
+	void Update()
+	{
+		UpdateWaves(WaterParticle.waveCollisionCountAvg);
+	}
+
+	private void UpdateWaves(int count)
+	{
+		if (count > 0 && count < wavesTwoSwitchCount)
+		{
+			wavesOne.volume += Time.deltaTime / fadeTime;
+			wavesTwo.volume -= Time.deltaTime / fadeTime;
+			wavesThree.volume -= Time.deltaTime / fadeTime;
+		}
+		else if (count > wavesTwoSwitchCount && count < wavesThreeSwitchCount)
+		{
+			wavesOne.volume -= Time.deltaTime / fadeTime;
+			wavesTwo.volume += Time.deltaTime / fadeTime;
+			wavesThree.volume -= Time.deltaTime / fadeTime;
+		}
+		else if (count > wavesThreeSwitchCount)
+		{
+			wavesOne.volume -= Time.deltaTime / fadeTime;
+			wavesTwo.volume -= Time.deltaTime / fadeTime;
+			wavesThree.volume += Time.deltaTime / fadeTime;
+		}
+	}
+}
---
+++ MoonWaves/Assets/Scripts/WaterParticle.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterParticle : MonoBehaviour {
+
+	//Static stuff
+	private static bool newFrame;
+	private static int[] waveCollisionCountBuffer = new int[50];
+	private static int waveBufferIndex = 0;
+
+	public static int waveCollisionCount
+	{
+		get
+		{
+			return waveCollisionCountBuffer[waveBufferIndex];
+		}
+	}
+	public static int waveCollisionCountAvg
+	{
+		get
+		{
+			int sum = 0;
+			for(int i = 0; i < waveCollisionCountBuffer.Length; i++)
+			{
+				sum += waveCollisionCountBuffer[i];
+			}
+			return sum / waveCollisionCountBuffer.Length;
+		}
+	}
+	
+	//non-static stuff
+	public float waveHeightThreshold;
+
+	private int frameCollisionCount;
+
+	void Update()
+	{
+		newFrame = true;
+    }
+
+	void LateUpdate()
+	{
+		if (newFrame)
+		{
+			waveBufferIndex++;
+			if (waveBufferIndex == waveCollisionCountBuffer.Length - 1) waveBufferIndex = 0;
+
+			waveCollisionCountBuffer[waveBufferIndex] = 0;
+			newFrame = false;
+		}
+
+		if (transform.position.sqrMagnitude > waveHeightThreshold) waveCollisionCountBuffer[waveBufferIndex]++;
+		//waveCollisionCountBuffer[waveBufferIndex] += frameCollisionCount;
+		frameCollisionCount = 0;
+	}
+
+	//private void OnCollisionStay2D(Collision2D collision)
+	//{
+	//	if (transform.position.sqrMagnitude > waveHeightThreshold) frameCollisionCount++;
+	//}
+}
