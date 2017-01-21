48dcb1c043c76d05e33cdbb775cb9cb0d9187fa6
Julian Stengaard <julianstengaard@gmail.com>
2017-01-21 15:13:00 +0100

    added a moon

---
+++ MoonWaves/Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public GameObject LookTarget;
+
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(transform.position + Vector3.forward, transform.position - LookTarget.transform.position);
+	}
+}
