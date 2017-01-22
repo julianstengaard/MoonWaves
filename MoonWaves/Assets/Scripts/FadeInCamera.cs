4411adf8aca7ce45b084222e98a5af5b870b2d15
Julian Stengaard <julianstengaard@gmail.com>
2017-01-22 12:33:06 +0100

    intro menu

---
+++ MoonWaves/Assets/Scripts/FadeInCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Holoville.HOTween;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class FadeInCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    VignetteAndChromaticAberration script = GetComponent<VignetteAndChromaticAberration>();
+	    HOTween.To(script, 1.5f, "intensity", 0f, false, EaseType.EaseInCubic, 0f);
+        HOTween.To(script, 1.5f, "blur", 0f, false, EaseType.EaseInCubic, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
